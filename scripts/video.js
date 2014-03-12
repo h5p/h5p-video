fce122fb6e609f75f185f267b17c42da000caf57
Frode Petterson <frode.petterson@amendor.com>
2014-03-12 16:20:22 +0100

    HFJ-187: Fixed copyrights for cp, audio and image.

--- scripts/video.js
+++ scripts/video.js
@@ -29,6 +29,18 @@ if (H5P.getPath === undefined) {
 H5P.Video = function (params, id) {
   this.params = params;
   this.contentId = id;
+  
+  // Use new copyright information if available. Fallback to old.
+  if (params.files !== undefined
+      && params.files[0] !== undefined
+      && params.files[0].copyrights !== undefined
+      && params.files[0].copyrights.length) {
+      
+    this.copyrights = params.files[0].copyrights;
+  }
+  else if (params.copyright !== undefined) {
+    this.copyrights = [params.copyright];
+  }
 };
 
 // For android specific stuff.
@@ -549,3 +561,15 @@ H5P.Video.prototype.getPreferredQuality = function () {
     return this.qualities[level].source;
   }
 }
+
+
+/**
+ * Gather copyright information for the current content.
+ *
+ * @returns {Object} Copyright information
+ */
+H5P.Video.prototype.getCopyrights = function () {
+  return {
+    copyrights: H5P.getCopyrightList(this.copyrights)
+  };
+};
