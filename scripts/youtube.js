64970eef551fe5bb71a239750d2cbf4bda4d94e2
Thomas Marstrander <thomasmars87@gmail.com>
2015-05-08 12:01:46 +0200

    Added youtube class to video container, to be able to specifically style content on top of youtube.

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -105,7 +105,7 @@ H5P.VideoYouTube = (function ($) {
     * @param {jQuery} $container
     */
     self.appendTo = function ($container) {
-      $container.append($wrapper);
+      $container.addClass('youtube').append($wrapper);
 
       if (player === undefined) {
         if (window.YT === undefined) {
