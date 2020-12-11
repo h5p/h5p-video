b1182cddfdeffbc0b22b6693a11b95128229854e
Frode Petterson <frode.andre.petterson@gmail.com>
2020-12-11 17:16:54 +0100

    Add Panopto autoload and autopause

--- scripts/panopto.js
+++ scripts/panopto.js
@@ -65,6 +65,7 @@ H5P.VideoPanopto = (function ($) {
           onIframeReady: function () {
             $placeholder.children(0).text('');
             self.trigger('loaded');
+            player.loadVideo();
           },
           onReady: function () {
             self.trigger('loaded');
@@ -82,6 +83,7 @@ H5P.VideoPanopto = (function ($) {
 
               self.trigger('captions', captions);
             }
+            self.pause();
           },
           onStateChange: function (state) {
             // TODO: Playback rate fix for IE11?
