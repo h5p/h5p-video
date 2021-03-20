f70bcd88df061e9c2397e3ce8f990ee1d3865447
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2021-03-20 12:30:51 +0100

    Nanootv: Adjust matching of nanoo.tv urls

--- scripts/nanoo-tv.js
+++ scripts/nanoo-tv.js
@@ -423,9 +423,9 @@ H5P.VideoNanooTv = (function ($) {
    */
 
   var getPath = function (url) {
-    var matches = url.match(/(nanoo.tv\/link\/(v|w|W)\/)([A-Za-z0-9_-]+)/i);
-    if (matches && matches[3]) {
-      return "https:\/\/nanoo.tv\/link\/W\/".concat(matches[3]);
+    var matches = url.match(/nanoo.tv\/link\/[npvw]\/([A-Za-z0-9_-]+)/i);
+    if (matches && matches[1]) {
+      return "https:\/\/nanoo.tv\/link\/W\/".concat(matches[1]);
     }
   };
 
