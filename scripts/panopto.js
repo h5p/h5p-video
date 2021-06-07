46bf6dea9385736bc985f06d5b8f9edc9308b44b
Ravi Majithia <ravikiran.majithia@joubel.com>
2021-06-07 10:34:18 +0200

    Revert "Add Stop method for the Pronopto video"
    This reverts commit b039a421b5b3491a6bf2637b56eb39628acfdfc0.

--- scripts/panopto.js
+++ scripts/panopto.js
@@ -167,18 +167,6 @@ H5P.VideoPanopto = (function ($) {
     };
 
     /**
-     * Stop the video.
-     *
-     * @public
-     */
-     self.stop = function () {
-      if (!player || !player.playVideo) {
-        return;
-      }
-      player.stopVideo();
-    };
-
-    /**
      * Pause the video.
      *
      * @public
