24de2b7285bdec8de70bfd70c0ac382cbc4cd69f
makmentins <maris.akmentins@h5p.group>
2023-10-11 19:54:45 +0200

    JI-5549 seek and pause on resetTask

--- scripts/video.js
+++ scripts/video.js
@@ -176,6 +176,8 @@ H5P.Video = (function ($, ContentCopyrights, MediaCopyright, handlers) {
     self.resetTask = function () {
       self.WAS_RESET = true;
       delete self.oldTime;
+      self.seek(parameters.startAt || 0);
+      self.pause();
     };
 
     // Resize the video when we know its aspect ratio
