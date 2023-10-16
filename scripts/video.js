a706f7dfcac7e0c51cce52466f3f6a1b4163b9ea
devland <adevland@gmail.com>
2023-10-16 15:07:37 +0200

    JI-5559 simplify resetTas code

--- scripts/video.js
+++ scripts/video.js
@@ -184,18 +184,11 @@ H5P.Video = (function ($, ContentCopyrights, MediaCopyright, handlers) {
 
     // Resize the video when we know its aspect ratio
     self.on('loaded', function () {
-      if (self.WAS_RESET) {
-        self.seek(parameters.startAt || 0);
-        if (!parameters.playback.autoplay) {
-          self.pause();
-        }
-        self.WAS_RESET = false;
-      } else if (self.oldTime) {
-        self.seek(self.oldTime);
-        if (!parameters.playback.autoplay) {
-          self.pause();
-        }
+      self.seek(self.WAS_RESET ? parameters.startAt || 0 : self.oldTime);
+      if (!parameters.playback.autoplay) {
+        self.pause();
       }
+      self.WAS_RESET = false;
 
       self.trigger('resize');
     });
