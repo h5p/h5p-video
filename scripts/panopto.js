fd14bc7d0cd4ea9fe0c3b180936e2654c970d649
Frode Petterson <frode.andre.petterson@gmail.com>
2026-05-19 09:04:07 +0200

    Revert "VA-721 remove panopto wrapper whitespace via font-size 0 (#152)"
    This reverts commit e26e1857a8c20df5eccbacbbbf6fcd286a84708b.

--- scripts/panopto.js
+++ scripts/panopto.js
@@ -470,21 +470,27 @@ H5P.VideoPanopto = (function ($) {
         create();
         return;
       }
-      
-      $wrapper.removeAttr('font-size');
 
-      let width = $wrapper[0].clientWidth;
-      let height = options.fit ? $wrapper[0].clientHeight : (width * (9/16));
-      
+      // Use as much space as possible
+      $wrapper.css({
+        width: '100%',
+        height: '100%'
+      });
+
+      var width = $wrapper[0].clientWidth;
+      var height = options.fit ? $wrapper[0].clientHeight : (width * (9/16));
+
+      // Set size
+      $wrapper.css({
+        width: width + 'px',
+        height: height + 'px'
+      });
+
       const $iframe = $placeholder.children('iframe');
       if ($iframe.length) {
         $iframe.attr('width', width);
         $iframe.attr('height', height);
       }
-      
-      $wrapper.css({
-        'font-size': 0
-      });
     });
 
     let currentTrack;
