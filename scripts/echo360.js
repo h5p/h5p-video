2ace553aa9fbb686178cd8f6d919a90b6e69fa3b
Frode Petterson <frode.andre.petterson@gmail.com>
2026-07-28 14:21:19 +0200

    VA-1801 Fix Echo360

--- scripts/echo360.js
+++ scripts/echo360.js
@@ -306,7 +306,7 @@ H5P.VideoEchoVideo = (() => {
         queryString += `startTimeMillis=${Math.round(options.startAt * 1000)}&`;
       }
 
-      wrapperElement.innerHTML = `<iframe src="${sources[0].path}${queryString}" style="display: inline-block; width: 100%; height: 100%;" allow="autoplay; fullscreen" frameborder="0" scrolling="no"></iframe>`;
+      wrapperElement.innerHTML = `<iframe src="${encodeURI(sources[0].path + queryString)}" style="display: inline-block; width: 100%; height: 100%;" allow="autoplay; fullscreen" frameborder="0" scrolling="no"></iframe>`;
       player = wrapperElement.firstChild;
       // Create a new player
       registerEchoPlayerEventListeneners(player);
